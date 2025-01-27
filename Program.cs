1628ccdd2610a1af3f52c52a37d17ea3da7d1117
Kohhau <koh.hau5307@gmail.com>
2025-01-27 16:01:33 +0800

    Student name comment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using Assignment;
+﻿//=============================
+// Student Number: S10266766
+// Student Name: Koh Hau
+// Partner Name: Haziq Hairil 
+//=============================
+using Assignment;
 
 void CreateAirlines(Terminal terminal)
 {
